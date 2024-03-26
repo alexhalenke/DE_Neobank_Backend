68c9ee8b0a1b695c020fd2a2cfff3df43aea43bb
Alexander Halenke <ahalenke@hotmail.com>
2024-03-26 22:08:04 +0100

    update

---
+++ de_neobank_backend/dags/airflow_code.py
@@ -0,0 +1,25 @@
+import os
+
+from airflow import DAG
+
+import pendulum
+
+from airflow.operators.bash import BashOperator
+
+
+DBT_DIR = os.getenv("DBT_DIR")
+
+
+with DAG(
+    "dbt_basics",
+    default_args={
+        "depends_on_past": True,
+    },
+    start_date=pendulum.today("UTC").add(days=-1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        bash_command=f"dbt run --project-dir {DBT_DIR}",
+    )
