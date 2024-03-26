0118ef2304bd8d48adbe4d0247ae27b53f187684
Alexander Halenke <ahalenke@hotmail.com>
2024-03-26 22:02:58 +0100

    alexhalenke_26032024 hopefully it solves the error

--- dbt_projectNeo/models/staging/src_devices.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='table') }}
-select
- cast( string_field_0 as varchar(250)) as device_type
-,   cast(replace(string_field_1,'user_', '') as integer) as user_id
-
-from {{ ref('src_bronzetiers') }}
-where type ="devices"
