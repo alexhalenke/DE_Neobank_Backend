dac02311120d8e5ca3ea735c6df55c1040d3f676
Alexander Halenke <ahalenke@hotmail.com>
2024-03-26 20:04:50 +0100

    alexhalenke_26032024 silver layer schema is almost done. Transaction fact table and device notivication dimension table is missing

--- dbt_projectNeo/models/02_silver2/device_notification_dimensions.sql
+++ dbt_projectNeo/models/02_silver2/device_notification_dimensions.sql
@@ -1,9 +1,4 @@
 {{ config(materialized='table') }}
 
 SELECT
-  REGEXP_REPLACE(CAST(user_id AS STRING), '^user_', '') AS user_id,
-  FORMAT_TIMESTAMP('%Y-%m-%d', TIMESTAMP(created_date)) AS created_date,
-  CONCAT(FORMAT_TIMESTAMP('%H:%M:%S', TIMESTAMP(created_date)), ' ', FORMAT_TIMESTAMP('%Z', TIMESTAMP(created_date))) AS date_time_with_timezone,
-  * EXCEPT (user_id, created_date) -- Exclude the columns already transformed
 FROM
-  `Bronze_Tier.users`
--- dbt_projectNeo/models/02_silver2/transactions_facts.sql
+++ dbt_projectNeo/models/02_silver2/transactions_facts.sql
@@ -1,7 +1,4 @@
 {{ config(materialized='table') }}
 
 select
-  REPLACE(CAST(string_field_0 AS STRING), 'string_field_0', '') as device_type,
-  REPLACE(REGEXP_REPLACE(CAST(string_field_1 AS STRING), '^string_field_1', ''), 'user_id', '') as user_id
 from
-  `Bronze_Tier.devices`
