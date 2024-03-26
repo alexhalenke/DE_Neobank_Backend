beee293a4d35047f1fe4db47cb432e7274406963
Alexander Halenke <ahalenke@hotmail.com>
2024-03-26 19:38:16 +0100

    alexhalenke_26032024 here yo9u have the dbt transformations and almost all the schema for the silver layer

--- dbt_projectNeo/models/02_silver/device_table.sql
+++ dbt_projectNeo/models/02_silver/device_table.sql
@@ -1,7 +1,9 @@
 {{ config(materialized='table') }}
 
-select
-  REPLACE(CAST(string_field_0 AS STRING), 'string_field_0', '') as device_type,
-  REPLACE(REGEXP_REPLACE(CAST(string_field_1 AS STRING), '^string_field_1', ''), 'user_id', '') as user_id
-from
+SELECT
+  string_field_0 AS device_type,
+  REGEXP_REPLACE(CAST(string_field_1 AS STRING), '^user_', '') AS user_id
+FROM
   `Bronze_Tier.devices`
+WHERE
+  string_field_0 != 'string_field_0' AND string_field_1 != 'string_field_1'
