2bd03b464a4fc5c665310d4cad965c7eee639ae3
luiggi navilys <luigginavilys@msn.com>
2024-04-02 18:07:52 +0000

    minor error on amt trx mart

--- dbt_projectNeo/models/03_mart/finance/amt_trx_mart.sql
+++ dbt_projectNeo/models/03_mart/finance/amt_trx_mart.sql
@@ -13,4 +13,8 @@
  FROM  {{ ref('transactions_facts') }} as t
  left join  {{ ref('date_dim') }}  as d
  on CAST(FORMAT_DATE('%Y%m%d', cast(t.created_date as date)) AS INT64) = d.date_ID
- group by d.year,t.transactions_type
+ group by
+  d.year,
+  t.transactions_type,
+  t.direction,
+  d.month
