1d1150533efc1485732490b61d91771ebd4337d4
MainlyMarlin <115432345+MainlyMarlin@users.noreply.github.com>
2024-04-02 14:14:26 +0100

    Added column 'month' to moving_avg_trx_mart.sql

--- dbt_projectNeo/models/03_mart/finance/moving_avg_trx_mart.sql
+++ dbt_projectNeo/models/03_mart/finance/moving_avg_trx_mart.sql
@@ -6,6 +6,7 @@
 SELECT
 
   sum(t.amount_usd) as total_amount,
+  t.direction,
   d.year,
   d.month
 
@@ -13,7 +14,7 @@ SELECT
  FROM {{ ref('transactions_facts') }} as t
  left join {{ ref( 'date_dim') }}  as d
  on CAST(FORMAT_DATE('%Y%m%d', cast(t.created_date as date)) AS INT64) = d.date_ID
- group by d.year, d.month
+ group by d.year, d.month, t.direction
 
 
  )
@@ -24,3 +25,4 @@ select
 
  AVG(total_amount) OVER (ORDER BY a.year,a.month ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS moving_avg
  from agg_amount_by_month a
+
