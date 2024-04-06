f79eace23ca7986c5f684f2a458cca08b35c88a2
luiggi navilys <luigginavilys@msn.com>
2024-04-06 01:31:08 +0000

    fix moving average

--- dbt_projectNeo/models/03_mart/finance/moving_avg_trx_mart.sql
+++ dbt_projectNeo/models/03_mart/finance/moving_avg_trx_mart.sql
@@ -8,14 +8,13 @@ SELECT
   sum(t.amount_usd) as total_amount,
   t.direction,
   d.year,
-  d.month,
-  t.transactions_state
+  d.month
 
 
  FROM {{ ref('transactions_facts') }} as t
  left join {{ ref( 'date_dim') }}  as d
  on CAST(FORMAT_DATE('%Y%m%d', cast(t.created_date as date)) AS INT64) = d.date_ID
- group by d.year, d.month, t.direction,  t.transactions_state
+ group by d.year, d.month, t.direction
 
 
  )
@@ -24,6 +23,7 @@ select
   a.year,
   a.month,
   a.direction,
-  a.transactions_state,
- AVG(total_amount) OVER (ORDER BY a.year,a.month ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS moving_avg
+
+
+ AVG(total_amount) OVER (PARTITION BY a.direction ORDER BY a.year,a.month ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS moving_avg
  from agg_amount_by_month a
